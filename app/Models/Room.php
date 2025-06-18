1956a0fc58711719622fd029548a4edcee2cd8fb
Farhan Sultan Nadlir <sultannadlir.11@gmail.com>
2025-06-18 10:20:30 +0700

    adding room tables to db

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -2,9 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Room extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'boarding_house_id',
+        'name',
+        'room_type',
+        'square_feet',
+        'price_per_month',
+        'is_available',
+    ];
+
+    public function boardingHouse()
+    {
+        return $this->belongsTo(BoardingHouse::class);
+    }
 }
